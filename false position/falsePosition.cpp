8bec1ea65409114b1875e729f695a0b400bdc219
BekLg <bereketlegesse22@gmail.com>
2023-04-04 21:34:44 +0300

    false position gets fully functional

--- false position/falsePosition.cpp
+++ false position/falsePosition.cpp
@@ -3,16 +3,37 @@
 
 using namespace std;
 
-double customFunction(double c) {
-    return cos(c) - c * exp(c);
-}
-void falsePosition(double a, double b, double tol)
+// Function to find the root using the False Position Method
+double falsePosition(double a, double b, double TOL, int N0, double (*customFunction)(double))
 {
-    double c= ((a * customFunction(b)) -  (b * customFunction(a))) / (b-a);
-    cout<<c;
+    double c = customFunction(a), d = customFunction(b), e = 0;
+    for (int i = 2; i <= N0; ++i) {  // Loop through maximum number of iterations
+
+         e= ((a * d) -  (b * c)) / (b-a); // Compute new approximation using false position formula
+        if (fabs(e - b) < TOL) return e;
+        double f_e = customFunction(e);  // Compute function value at new approximation
+        if (f_e * d < 0) a = b, c = d;
+        b = e, d = f_e;
+    }
+    throw runtime_error("Method failed after " + to_string(N0) + " iterations.");  // Throw exception if maximum number of iterations is reached
+}
+
+// Example usage
+double customFunction(double x)
+{
+    return x * x - 3 * x + 2;  // Define example function
 }
 
 int main()
 {
-    falsePosition(1, -2.17798, 0.001);
+    try {
+        double a = 0, b = 2, TOL = 0.0001;
+        int N0 = 100;
+        double root = falsePosition(a, b, TOL, N0, customFunction);
+        cout << "The root is approximately: " << root << endl;
+    }
+    catch (const exception& e) {
+        cerr << e.what() << endl;  // Print error message to standard error
+    }
+    return 0;
 }
